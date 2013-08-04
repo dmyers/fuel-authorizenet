d325abb052547837d992ab3d58f4340c4660b0d0
Derek Myers <arcticpro@gmail.com>
2013-08-04 15:06:08 -0500

    Removed old autoloader code in bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -2,13 +2,6 @@
 
 require_once __DIR__ . DS . 'vendor' . DS . 'authorizenet-sdk' . DS . 'AuthorizeNet.php';
 
-Autoloader::add_core_namespace('AuthorizeNet');
-
-Autoloader::add_classes(array(
-	'AuthorizeNet\\AuthorizeNet'          => __DIR__.'/classes/authorizenet.php',
-	'AuthorizeNet\\AuthorizeNetException' => __DIR__.'/classes/authorizenet.php',
-));
-
 Config::load('authorizenet', true);
 
 $api_login_id    = \Config::get('authorizenet.api_login_id');
