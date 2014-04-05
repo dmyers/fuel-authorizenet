201514849810d134a34cae0b6b599a85ae6be614
Derek Myers <arcticpro@gmail.com>
2014-04-05 17:28:08 -0500

    Changed defining named constants to be optional if config are not set to allow defining manually

--- bootstrap.php
+++ bootstrap.php
@@ -8,6 +8,14 @@ $api_login_id    = \Config::get('authorizenet.api_login_id');
 $transaction_key = \Config::get('authorizenet.transaction_key');
 $sandbox         = \Config::get('authorizenet.sandbox', false);
 
-define("AUTHORIZENET_API_LOGIN_ID", $api_login_id);
-define("AUTHORIZENET_TRANSACTION_KEY", $transaction_key);
-define("AUTHORIZENET_SANDBOX", $sandbox);
+if (!empty($api_login_id)) {
+	define('AUTHORIZENET_API_LOGIN_ID', $api_login_id);
+}
+
+if (!empty($transaction_key)) {
+	define('AUTHORIZENET_TRANSACTION_KEY', $transaction_key);
+}
+
+if (!empty($sandbox)) {
+	define('AUTHORIZENET_SANDBOX', $sandbox);
+}
